8fab5ace9fc880d84c0aeb77d259d9dde88dc0ef
hitswint <wgq_hit@126.com>
2024-10-29 01:15:49 +0800

    Fixed urls.py.

--- AC/urls.py
+++ AC/urls.py
@@ -1,11 +1,11 @@
-from django.urls import path
+from django.urls import path, re_path
 from AC.views import index, OtaVersionView, OtaUpdateView
 # from swint.models import Article, Category
 
 urlpatterns = [
-    path(r'^$', index, name='index-view'),
-    path(r'^Fota/image.version$',
+    re_path(r'^$', index, name='index-view'),
+    re_path(r'^Fota/image.version$',
         OtaVersionView.as_view(),
         name='ota-version-view'),
-    path(r'^Fota/image.bin$', OtaUpdateView.as_view(), name='ota-update-view'),
+    re_path(r'^Fota/image.bin$', OtaUpdateView.as_view(), name='ota-update-view'),
 ]
--- AccessControlSystem/urls.py
+++ AccessControlSystem/urls.py
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.contrib import admin
 
 urlpatterns = [
-    path(r'^admin/', admin.site.urls),
-    path(r'', include('AC.urls')),
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'', include('AC.urls')),
 ]
